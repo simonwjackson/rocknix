e0f157762d37f468cf3e740452d2c3c0ec1cdcb5
Simon W. Jackson <simon@simonwjackson.io>
2026-05-06 16:15:38 -0600

    fix(nix): build Layer 10b rootfs from host systems

--- projects/ROCKNIX/packages/tools/nix-integration/guest/flake.nix
+++ projects/ROCKNIX/packages/tools/nix-integration/guest/flake.nix
@@ -7,36 +7,43 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "aarch64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      targetSystem = "aarch64-linux";
+      hostSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllHostSystems = nixpkgs.lib.genAttrs hostSystems;
       configuration = nixpkgs.lib.nixosSystem {
-        inherit system;
+        system = targetSystem;
         modules = [ ./rocknix-guest.nix ];
       };
       toplevel = configuration.config.system.build.toplevel;
-      closure = pkgs.closureInfo {
-        rootPaths = [ toplevel ];
-      };
-      rootfs = pkgs.runCommand "rocknix-layer10b-guest-rootfs" {
-        nativeBuildInputs = [ pkgs.coreutils pkgs.gnutar pkgs.zstd ];
-      } ''
-        mkdir -p root/nix/store root/sbin root/tmp root/proc root/sys root/dev root/run $out/tarball
-        chmod 1777 root/tmp
-        while IFS= read -r store_path; do
-          cp -a "$store_path" root/nix/store/
-        done < ${closure}/store-paths
-        ln -s ${toplevel}/init root/init
-        ln -s ${toplevel}/init root/sbin/init
-        ln -s ${toplevel}/etc root/etc
-        tar --sort=name --numeric-owner --owner=0 --group=0 --zstd \
-          -cf $out/tarball/rocknix-layer10b-guest-rootfs-aarch64-linux.tar.zst \
-          -C root .
-      '';
+      mkRootfs = hostSystem:
+        let
+          pkgs = import nixpkgs { system = hostSystem; };
+          closure = pkgs.closureInfo {
+            rootPaths = [ toplevel ];
+          };
+        in pkgs.runCommand "rocknix-layer10b-guest-rootfs" {
+          nativeBuildInputs = [ pkgs.coreutils pkgs.gnutar pkgs.zstd ];
+        } ''
+          mkdir -p root/nix/store root/sbin root/tmp root/proc root/sys root/dev root/run $out/tarball
+          chmod 1777 root/tmp
+          while IFS= read -r store_path; do
+            cp -a "$store_path" root/nix/store/
+          done < ${closure}/store-paths
+          ln -s ${toplevel}/init root/init
+          ln -s ${toplevel}/init root/sbin/init
+          ln -s ${toplevel}/etc root/etc
+          tar --sort=name --numeric-owner --owner=0 --group=0 --zstd \
+            -cf $out/tarball/rocknix-layer10b-guest-rootfs-aarch64-linux.tar.zst \
+            -C root .
+        '';
     in {
       nixosConfigurations.rocknix-guest = configuration;
-      packages.${system} = {
-        rootfs = rootfs;
-        default = self.packages.${system}.rootfs;
-      };
+      packages = forAllHostSystems (hostSystem:
+        let
+          rootfs = mkRootfs hostSystem;
+        in {
+          inherit rootfs;
+          default = rootfs;
+        });
     };
 }
